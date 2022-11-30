71ccb6fe3dd527332eb0248f8b10519624327214
Kamil <majcherczak.kamil@protonmail.com>
2022-11-30 23:16:08 +0100

    commit

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 import TicTacToe from "./ticTacToe.js";
 
+const message = document.querySelector("#text");
 const board = document.querySelector(".board");
 const cells = document.querySelectorAll(".cell");
 
-const game = new TicTacToe(board, cells);
-console.dir(cells);
+const game = new TicTacToe(board, cells, message);
 cells.forEach((cell) => {
   cell.addEventListener("click", () => {
     game.cellClick(cell);
-    console.log(cell)
   });
 });
-
-
--- ticTacToe.js
+++ ticTacToe.js
@@ -1,19 +1,45 @@
 export default class TicTacToe {
-  constructor(board, cells) {
+  constructor(board, cells, message) {
     this.board = board;
     this.cell = cells;
+    this.message = message;
   }
   winingConbinations = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
   ];
 
+  cellClick(targetedCell) {
+    if (
+      this.board.classList.contains("player-circle-turn") &&
+      !(
+        targetedCell.classList.contains("x") ||
+        targetedCell.classList.contains("circle")
+      )
+    ) {
+      targetedCell.classList.add("circle");
+    } else if (
+      this.board.classList.contains("player-x-turn") &&
+      !(
+        targetedCell.classList.contains("x") ||
+        targetedCell.classList.contains("circle")
+      )
+    ) {
+      targetedCell.classList.add("x");
+    } else {
+      return;
+    }
+    if(this.checkForWinConditions()) this.gameEnds(false);
+    if(this.draw()) this.gameEnds(true);
+
+    this.changeCurrentPlayer();
+  }
 
   changeCurrentPlayer() {
     if (this.board.classList.contains("player-circle-turn"))
@@ -33,23 +59,44 @@ export default class TicTacToe {
     });
   }
 
-  cellClick(targetedCell) {
-    if (this.board.classList.contains("player-circle-turn"))
-      targetedCell.classList.add("circle");
-    if (this.board.classList.contains("player-x-turn"))
-      targetedCell.classList.add("x");
-    this.changeCurrentPlayer();
-  }
-
   gameReset() {
     this.claerBoard();
     if (this.board.classList.contains("player-x-turn")) return;
     this.changeCurrentPlayer();
   }
 
-  checkingForWinConditions(){
-    
-      targetedCell.parentNode.childNodes
-    
+  checkForWinConditions() {
+    let currentClass;
+    if (this.board.classList.contains("player-x-turn")) currentClass = "x";
+    if (this.board.classList.contains("player-circle-turn"))
+      currentClass = "circle";
+    return this.winingConbinations.some((element) => {
+      return element.every((index) => {
+        return this.cell[index].classList.contains(currentClass);
+      });
+    });
+  }
+  draw() {
+    return [...this.cell].every((element) => {
+      return (
+        element.classList.contains("x") || element.classList.contains("circle")
+      );
+    });
+  }
+
+  gameEnds(draw) {
+    if (draw) {
+      console.log("draw");
+      this.message.innerText = "It's a draw";
+    } else {
+      if (this.board.classList.contains("player-x-turn")) {
+        console.log("x win");
+        this.message.innerText = "X win";
+      }
+      if (this.board.classList.contains("player-circle-turn")) {
+        console.log("circle win");
+        this.message.innerText = "Circle win";
+      }
+    }
   }
 }
